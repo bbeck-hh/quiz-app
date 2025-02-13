c5cb7c8dfb00d8a4c363bd483ba82de0fa2272e8
Bjoern <beck.b.hh@gmail.com>
2025-02-13 21:45:04 +0100

    Add bookmark functionality and improve answer visibility toggle in bookmarks.html

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 const btnAnswer = document.querySelector('[data-js="btn-show-answer"]');
 const answerShown = document.querySelector('[data-js="answer-shown"]');
 
+const btnBookmark = document.querySelector('[data-js="btn-bookmark"]');
+const bookmarkFlag = document.querySelector('[data-js="svgElement"]')
 
 btnAnswer.addEventListener('click', () => {
     answerShown.classList.toggle('visible');
 });
 
+btnBookmark.addEventListener('click', () => {
+    bookmarkFlag.classList.toggle('bookmark__icon---active');
+})
