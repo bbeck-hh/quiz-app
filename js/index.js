4fda11f44ea35a460e9468d5f779a67a568b527b
Bjoern <beck.b.hh@gmail.com>
2025-02-14 09:06:16 +0100

    not finished event Listner

--- js/index.js
+++ js/index.js
@@ -1,13 +1,30 @@
 const btnAnswer = document.querySelector('[data-js="btn-show-answer"]');
 const answerShown = document.querySelector('[data-js="answer-shown"]');
+const questionId = btnAnswer.getAttribute('data-js-questionId');
+const answerId = answerShown.getAttribute('data-js-answerId');
+
 
 const btnBookmark = document.querySelector('[data-js="btn-bookmark"]');
 const bookmarkFlag = document.querySelector('[data-js="svgElement"]')
 
-btnAnswer.addEventListener('click', () => {
-    answerShown.classList.toggle('visible');
-});
+
+// btnAnswer.addEventListener('click', () => {
+//     console.log(`btnAnswer clicked und die Fragenummer: ${questionId}`);
+
+//     answerShown.classList.toggle('visible');
+// });
 
 btnBookmark.addEventListener('click', () => {
+    console.log(`btnBookmark clicked`);
     bookmarkFlag.classList.toggle('bookmark__icon---active');
 })
+
+document.addEventListener('click', event => {
+    console.log(`btnAnswer clicked\ndata-js-questionId: ${questionId}\ndata-js-answerId: ${answerId}`);
+
+    if (questionId === answerId) {
+        answerShown.classList.toggle('visible');
+    } else {
+        console.log(`Unterschiedliche Nummern`);
+    }
+});
