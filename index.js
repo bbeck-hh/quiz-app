35e82e6131a1b9535a31357d0f5f581b88a2cdac
Bjoern <beck.b.hh@gmail.com>
2025-02-19 12:01:53 +0100

    Implement interactive features for bookmarking and answer toggling

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // phew… not a lot going on here. Please add some code!
+// Be lazy ;-)
+const toCreate = (element) => document.createElement(element);
+const addCss = (htmlElement, cssClass) => htmlElement.classList.add(cssClass);
+const fromDataJS = (dataJs) => document.querySelector(`[data-js=${dataJs}]`);
+
+// Variables Area
+const btnBookmark = fromDataJS("btn-bookmark");
+const btnShowAnswer = fromDataJS("btn-show-answer");
+const questionCard = fromDataJS("question-card");
+const answerCard = fromDataJS("answer-card");
+
+// EventListner Area
+btnBookmark.addEventListener('click', () => {
+    console.log(`btnBookmark clicked`);
+    btnBookmark.classList.toggle('bookmark--active');
+});
+
+
+btnShowAnswer.addEventListener("click", () => {
+    // Needed cause there are spaces inside
+    const btnText = btnShowAnswer.textContent.trim();
+
+    if (btnText === "Show answer") {
+        btnShowAnswer.textContent = "Hide answer";
+        questionCard.classList.toggle("hidden");
+        answerCard.style.display = "block";
+    } else {
+        btnShowAnswer.textContent = "Show answer";
+        questionCard.classList.toggle("hidden");
+        answerCard.style.display = "none";
+    }
+
+});
